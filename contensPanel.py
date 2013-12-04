d3b331bdfe479eb92dbd5a204d3cfb2c8d7cb94e
Kazufumi Onuma <k.o.system2@gmail.com>
2013-12-04 16:53:27 +0900

    Add: contens panel in contentsPanel.py

--- contensPanel.py
+++ contensPanel.py
@@ -3,32 +3,23 @@ import wx
 
 class LeftPanel(wx.Panel):
     def __init__(self, parent, id):
-        wx.Panel.__init__(self, parent, id, style=wx.BORDER_SUNKEN)
+        wx.Panel.__init__(self, parent, id)
 
-        self.text = parent.GetParent().rightPanel.text
-
-        button1 = wx.Button(self, -1, '+', (10, 10))
-        button2 = wx.Button(self, -1, '-', (10, 60))
-
-        self.Bind(wx.EVT_BUTTON, self.OnPlus, id=button1.GetId())
-        self.Bind(wx.EVT_BUTTON, self.OnMinus, id=button2.GetId())
-
-    def OnPlus(self, event):
-        value = int(self.text.GetLabel())
-        value = value + 1
-        self.text.SetLabel(str(value))
-
-    def OnMinus(self, event):
-        value = int(self.text.GetLabel())
-        value = value - 1
-        self.text.SetLabel(str(value))
+        self.textCtrl = wx.TextCtrl(self, -1, style=wx.TE_MULTILINE)
+        box = wx.BoxSizer()
+        box.Add(self.textCtrl, 1, wx.EXPAND, 0)
+        self.SetSizer(box)
+        self.textCtrl.AppendText('1234 56 7890123 45678901234567912345679')
+        print self.textCtrl.GetChildren()
 
 
-class RightPanel(wx.Panel):
-    def __init__(self, parent, id):
-        wx.Panel.__init__(self, parent, id, style=wx.BORDER_SUNKEN)
+class RulerPanel(wx.Panel):
+    def __init__(self, parent, id, style=0):
+        wx.Panel.__init__(self, parent, id)
+        self.style = style
         self.text = wx.StaticText(self, -1, '0', (40, 60))
         self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.SetMinSize((20, 50))
 
     def OnPaint(self, event):
         # anti aliasing
@@ -39,14 +30,32 @@ class RightPanel(wx.Panel):
             dc = pdc
 
         dc.SetPen(wx.Pen('blue'))
-        for i in range(10):
-            y = 20*i + 1
-            dc.DrawLine(0, y, 200, y)
-        for i in range(11):
-            x = 20*i
-            dc.DrawLine(x, 0, x, 180)
-        dc.SetPen(wx.Pen('red'))
-        dc.DrawLine(0, 0, 200, 180)
+
+        if self.style == 1:  # Top
+            dc.DrawLine(0, 15, self.Size[0], 15)
+            dc.DrawLine(self.Size[0]/2, 15, self.Size[0]/2, self.Size[1])
+        elif self.style == 2:  # Mid
+            dc.DrawLine(self.Size[0]/2, 0, self.Size[0]/2, self.Size[1])
+            dc.DrawLine(self.Size[0]/2, 15, self.Size[0], 15)
+        elif self.style == 3:  # Bottom
+            dc.DrawLine(self.Size[0]/2, 0, self.Size[0]/2, 15)
+            dc.DrawLine(self.Size[0]/2, 15, self.Size[0], 15)
+        else:  # Single
+            dc.DrawLine(0, 15, self.Size[0], 15)
+
+
+class ContentsPanel(wx.Panel):
+    def __init__(self, parent, id, pos=(0, 0), size=(100, 50), style=0):
+        wx.Panel.__init__(self, parent, id, pos=pos, size=size)
+
+        rulerPanel = RulerPanel(self, -1, style=style)
+        textPanel = LeftPanel(self, -1)
+
+        hbox = wx.BoxSizer()
+        hbox.Add(rulerPanel, 0, wx.EXPAND, 0)
+        hbox.Add(textPanel, 1, wx.EXPAND | wx.TOP | wx.BOTTOM, 5)
+
+        self.SetSizer(hbox)
 
 
 class Communicate(wx.Frame):
@@ -54,15 +63,18 @@ class Communicate(wx.Frame):
         wx.Frame.__init__(self, parent, id, title, size=(380, 200))
 
         panel = wx.Panel(self, -1)
-        self.rightPanel = RightPanel(panel, -1)
 
-        leftPanel = LeftPanel(panel, -1)
+        ContentsPanel(panel, -1, pos=(0, 0), size=(200, 17*3+2+10))
+        ContentsPanel(panel, -1, pos=(200, 0), size=(100, 50), style=1)
+        contentsPanel = ContentsPanel(panel, -1, style=2)
+        contentsPanel.SetPosition((200, 50))
+        ContentsPanel(panel, -1, pos=(200, 100), size=(100, 17*5+2+10), style=3)
 
-        hbox = wx.BoxSizer()
-        hbox.Add(leftPanel, 1, wx.EXPAND | wx.ALL, 5)
-        hbox.Add(self.rightPanel, 1, wx.EXPAND | wx.ALL, 5)
+        # hbox = wx.BoxSizer()
+        # hbox.Add(rightPanel, 1, wx.EXPAND | wx.ALL, 0)
+        # hbox.Add(leftPanel, 1, wx.EXPAND | wx.ALL, 0)
 
-        panel.SetSizer(hbox)
+        # panel.SetSizer(hbox)
         self.Centre()
         self.Show(True)
 
