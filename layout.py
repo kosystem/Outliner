218df4afbef01d7685aabf840d17edaec21c64e3
kosystem <k.o.system2@gmail.com>
2013-12-17 22:08:06 +0900

    Fixes the design in layout.py

--- layout.py
+++ layout.py
@@ -14,14 +14,18 @@ class TextPanel(wx.Panel):
     def __init__(self, parent, id):
         wx.Panel.__init__(self, parent, id)
 
-        self.textCtrl = ExpandoTextCtrl(self, size=(parent.Size[0]-20, -1))
+        self.textCtrl = ExpandoTextCtrl(
+            self,
+            size=(parent.Size[0]-20, -1),
+            style=wx.BORDER_NONE |
+            wx.TE_NO_VSCROLL)
         # self.Bind(EVT_ETC_LAYOUT_NEEDED, self.OnRefit, self.textCtrl)
-        self.textCtrl.SetBackgroundColour((240, 240, 240))
+        self.textCtrl.SetBackgroundColour('#e0e0e0')
 
         box = wx.BoxSizer()
         box.Add(self.textCtrl, 1, wx.EXPAND, 0)
         self.SetSizer(box)
-        self.textCtrl.AppendText('1234 56 7890123 45678901234567912345679')
+        self.textCtrl.AppendText('1234 56 7890123 45678901234567912')
 
 
 class RulerPanel(wx.Panel):
@@ -40,57 +44,59 @@ class RulerPanel(wx.Panel):
         except:
             dc = pdc
 
-        dc.SetPen(wx.Pen('blue'))
+        dc.SetPen(wx.Pen('#a0a0a0'))
 
+        heightMargin = 10
+        width, height = self.Size[0], self.Size[1]
         if self.style == 1:  # Top
-            dc.DrawLine(0, 15, self.Size[0], 15)
-            dc.DrawLine(self.Size[0]/2, 15, self.Size[0]/2, self.Size[1])
+            dc.DrawLine(0, heightMargin, width, heightMargin)
+            dc.DrawLine(width/2, heightMargin, width/2, height)
         elif self.style == 2:  # Mid
-            dc.DrawLine(self.Size[0]/2, 0, self.Size[0]/2, self.Size[1])
-            dc.DrawLine(self.Size[0]/2, 15, self.Size[0], 15)
+            dc.DrawLine(width/2, 0, width/2, height)
+            dc.DrawLine(width/2, heightMargin, width, heightMargin)
         elif self.style == 3:  # Bottom
-            dc.DrawLine(self.Size[0]/2, 0, self.Size[0]/2, 15)
-            dc.DrawLine(self.Size[0]/2, 15, self.Size[0], 15)
+            dc.DrawLine(width/2, 0, width/2, heightMargin)
+            dc.DrawLine(width/2, heightMargin, width, heightMargin)
         else:  # Single
-            dc.DrawLine(0, 15, self.Size[0], 15)
+            dc.DrawLine(0, heightMargin, width, heightMargin)
 
 
 class ContentsPanel(wx.Panel):
     def __init__(self, parent, id, pos=(0, 0), size=(100, 50), style=0):
         wx.Panel.__init__(self, parent, id, pos=pos, size=size)
+        self.SetBackgroundColour('#e0e0e0')
 
         rulerPanel = RulerPanel(self, -1, style=style)
         textPanel = TextPanel(self, -1)
 
         hbox = wx.BoxSizer()
         hbox.Add(rulerPanel, 0, wx.EXPAND, 0)
-        hbox.Add(textPanel, 1, wx.EXPAND | wx.TOP | wx.BOTTOM, 5)
+        hbox.Add(textPanel, 1, wx.EXPAND | wx.TOP | wx.BOTTOM, 2)
 
         self.SetSizer(hbox)
 
-        self.Bind(EVT_ETC_LAYOUT_NEEDED, self.OnRefit)
+        self.Bind(EVT_ETC_LAYOUT_NEEDED, self.OnRefit, textPanel.textCtrl)
         self.OnRefit(EVT_ETC_LAYOUT_NEEDED)
 
     def OnRefit(self, evt):
         self.Fit()
-        print 'Contents:' , self.Size
-
+        # print 'Contents:', self.Size
 
 
 class TreeLayout(wx.Panel):
-    def __init__(self, parent, id, text):
+    def __init__(self, parent, id, text, style=0):
         wx.Panel.__init__(self, parent, id)
         self.SetBackgroundColour('#ededed')
         hbox = wx.BoxSizer(wx.HORIZONTAL)
         self.SetSizer(hbox)
 
-        contentPanel = ContentsPanel(self, -1, size=(200, -1))
+        contentPanel = ContentsPanel(self, -1, size=(200, -1), style=style)
 
         self.subPanel = wx.Panel(self, -1)
         self.subPanel.SetBackgroundColour(randColor())
 
-        hbox.Add(contentPanel, 0, wx.EXPAND | wx.ALL, 2)
-        hbox.Add(self.subPanel, 1, wx.EXPAND | wx.ALL, 2)
+        hbox.Add(contentPanel, 0, wx.EXPAND | wx.ALL, 0)
+        hbox.Add(self.subPanel, 1, wx.EXPAND | wx.ALL, 0)
 
         self.contents = wx.BoxSizer(wx.VERTICAL)
         self.subPanel.SetSizer(self.contents)
@@ -104,21 +110,21 @@ class TreeLayoutFrame(wx.Frame):
         panel.SetBackgroundColour(randColor())
         vbox = wx.BoxSizer(wx.VERTICAL)
 
-        midPan1 = TreeLayout(panel, -1, '1')
-        midPan2 = TreeLayout(panel, -1, '2')
+        midPan1 = TreeLayout(panel, -1, '1', style=1)
+        midPan2 = TreeLayout(panel, -1, '2', style=3)
         midPan11 = TreeLayout(midPan1.subPanel, -1, '11')
-        midPan21 = TreeLayout(midPan2.subPanel, -1, '21')
-        midPan22 = TreeLayout(midPan2.subPanel, -1, '22')
-        midPan23 = TreeLayout(midPan2.subPanel, -1, '22')
-        midPan24 = TreeLayout(midPan2.subPanel, -1, '22')
-        midPan1.contents.Add(midPan11, 1, wx.EXPAND | wx.ALL, 2)
-        midPan2.contents.Add(midPan21, 1, wx.EXPAND | wx.ALL, 2)
-        midPan2.contents.Add(midPan22, 1, wx.EXPAND | wx.ALL, 2)
-        midPan2.contents.Add(midPan23, 1, wx.EXPAND | wx.ALL, 2)
-        midPan2.contents.Add(midPan24, 1, wx.EXPAND | wx.ALL, 2)
+        midPan21 = TreeLayout(midPan2.subPanel, -1, '21', style=1)
+        midPan22 = TreeLayout(midPan2.subPanel, -1, '22', style=2)
+        midPan23 = TreeLayout(midPan2.subPanel, -1, '23', style=2)
+        midPan24 = TreeLayout(midPan2.subPanel, -1, '24', style=3)
+        midPan1.contents.Add(midPan11, 1, wx.EXPAND | wx.ALL, 0)
+        midPan2.contents.Add(midPan21, 1, wx.EXPAND | wx.ALL, 0)
+        midPan2.contents.Add(midPan22, 1, wx.EXPAND | wx.ALL, 0)
+        midPan2.contents.Add(midPan23, 1, wx.EXPAND | wx.ALL, 0)
+        midPan2.contents.Add(midPan24, 1, wx.EXPAND | wx.ALL, 0)
 
-        vbox.Add(midPan1, 0, wx.EXPAND | wx.ALL, 2)
-        vbox.Add(midPan2, 0, wx.EXPAND | wx.ALL, 2)
+        vbox.Add(midPan1, 0, wx.EXPAND | wx.ALL, 0)
+        vbox.Add(midPan2, 0, wx.EXPAND | wx.ALL, 0)
         panel.SetSizer(vbox)
         self.Centre()
         self.Show(True)
