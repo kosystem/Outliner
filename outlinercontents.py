c42bd1458f35f380cd887342b28d93a725c4452b
Kazufumi Onuma <k.o.system2@gmail.com>
2013-12-18 17:07:22 +0900

    Inprove OutlineContents for __str__ output

--- outlinercontents.py
+++ outlinercontents.py
@@ -1,4 +1,9 @@
 
+IMAGE_ALIGNMENT_BOTTOM = 0
+IMAGE_ALIGNMENT_TOP = 1
+IMAGE_ALIGNMENT_LEFT = 2
+IMAGE_ALIGNMENT_RIGHT = 3
+
 
 class OutlinerBase(object):
     def __init__(self):
@@ -18,35 +23,91 @@ class OutlinerBase(object):
         if index < len(self.contents):
             self.contents.pop(index)
 
+    def __str__(self):
+        returnStr = "{\n"
+        returnStr += ('"setting": ' + str(self.settings))
+        if len(self.contents) > 0:
+            returnStr += ', \n'
+            returnStr += '"contents": [\n'
+            for x in self.contents:
+                returnStr += str(x)
+                if x != self.contents[-1]:
+                    returnStr += ','
+                else:
+                    returnStr += ']\n'
+        else:
+            returnStr += '\n'
+
+        returnStr += '}\n'
+        return returnStr
+
 
 class OutlinerContents(object):
     """docstring for olContents"""
-    def __init__(self):
-        self.text = ''
+    def __init__(self, text=''):
+        self.text = text
         self.font = ''
-        self.fontSize = ''
+        self.fontSize = -1
         self.check = None
-        self.imageURL = ''
-        self.imageAlignments = 0  # bottom / top / L / R
         self.closed = False
         self.parent = None
+        self.imageURL = ''
+        self.imageAlignments = IMAGE_ALIGNMENT_BOTTOM
         self.subContents = []
 
     def appendSubContents(self, contents):
         self.subContents.append(contents)
+        contents.parent = self
 
     def insertSubContents(self, index, contents):
         if index >= len(self.subContents):
-            self.appendSubContents(contents)
+            self.subContents.append(contents)
         else:
-            self.appendSubContents.insert(index, contents)
+            self.subContents.insert(index, contents)
+        contents.parent = self
 
     def removeSubContetns(self, index):
         if index < len(self.subContents):
-            self.subContents.pop(index)
+            return self.subContents.pop(index)
+        else:
+            return False
+
+    def __str__(self):
+        returnStr = "{\n"
+        returnStr += ('"text": "' + self.text + '", \n')
+        returnStr += ('"font": "' + self.font + '", \n')
+        returnStr += ('"fontSize": ' + str(self.fontSize) + ', \n')
+        if self.check is None:
+            returnStr += '"check": null,\n'
+        else:
+            returnStr += ('"check": ' + str(self.check) + ', \n')
+        returnStr += ('"closed": ' + str(self.closed).lower() + ', \n')
+        returnStr += ('"imageURL": "' + self.imageURL + '", \n')
+        returnStr += ('"imageAlignments": ' + str(self.imageAlignments))
+        # returnStr += ('"parent": ' + self.parent + ', \n')
+        if len(self.subContents) > 0:
+            returnStr += ', \n'
+            returnStr += '"subContents": [\n'
+            for x in self.subContents:
+                returnStr += str(x)
+                if x != self.subContents[-1]:
+                    returnStr += ','
+                else:
+                    returnStr += ']\n'
+        else:
+            returnStr += '\n'
+
+        returnStr += '}\n'
+        return returnStr
 
 
 if __name__ == '__main__':
     base = OutlinerBase()
-    base.appendContents(OutlinerContents())
-
+    contents = OutlinerContents('1st contents')
+    contents.appendSubContents(OutlinerContents('sub contents 1'))
+    contents.appendSubContents(OutlinerContents('sub contents 2'))
+    contents.appendSubContents(OutlinerContents('sub contents 3'))
+    base.appendContents(contents)
+    temp = base.contents[0].removeSubContetns(1)
+    contents.insertSubContents(0, temp)
+    print base
